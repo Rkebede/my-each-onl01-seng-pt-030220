36715ba8c2c053d087b8b23134de7607b3fbc3d9
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-07 00:15:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(word)
+  letter = 0 
+  while letter <= word.length
+    word.length do |word|
+      letter += 1
+    word
+    end
+  end
 end
